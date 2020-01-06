522c4ee0c5e6ab771083c94b600e39f4eb95ec79
upaver20 <upa.ver.2.0@gmail.com>
2020-01-06 13:03:37 +0900

    channel id の変更

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ voice_client = ""
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-    channel_id = int(os.environ['LOG_CHANNEL_ID'])
+    channel_id = int(os.environ['CHANNEL_ID'])
     channel = client.get_channel(channel_id)
     await channel.send(content="おはようござい🍆")
 
@@ -19,7 +19,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.channel.id == int(os.environ['LOG_CHANNEL_ID']):
+    if message.channel.id == int(os.environ['CHANNEL_ID']):
         if client.user in message.mentions:
             if message.author.voice != None:
                 if message.content.endswith('join') and (client.voice_clients == []):
