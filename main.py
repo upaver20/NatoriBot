45606170bb723d703fbe70f30209ec22aa60baca
upaver20 <upa.ver.2.0@gmail.com>
2020-01-06 13:28:30 +0900

    少し見やすく

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import Sanabutton2Parser
 import downloader
 
 client = discord.Client()
-voice_client = ""
 
 @client.event
 async def on_ready():
@@ -20,19 +19,28 @@ async def on_message(message):
         return
 
     if message.channel.id == int(os.environ['CHANNEL_ID']):
+        bot = vc_warapper(client.voice_clients)
         if client.user in message.mentions:
             if message.author.voice != None:
-                if message.content.endswith('join') and (client.voice_clients == []):
+                if message.content.endswith('join') and not bot .is_in_vc:
                     await message.author.voice.channel.connect()
-                if message.content.endswith('leave') and (client.voice_clients != []):
-                    await client.voice_clients[0].disconnect()
+                if message.content.endswith('leave') and bot.is_in_vc:
+                    await bot.voice.disconnect()
         elif message.content != "":
             url = Sanabutton2Parser.Sanabutton2Parser(message.content)
             reply = f'{message.author.mention} {url}'
             await message.channel.send(reply)
-            if client.voice_clients[0].is_connected() and not client.voice_clients[0].is_playing():
+            if bot.voice.is_connected() and not bot.voice.is_playing():
                 downloader.download(url)
                 source = discord.FFmpegPCMAudio("button.mp3")
-                client.voice_clients[0].play(source)
+                bot.voice.play(source)
+
+class vc_warapper():
+    def __init__(self, voice_clients):
+        if voice_clients != []:
+            self.voice = voice_clients[0]
+            self.is_in_vc = True
+        else:
+            self.is_in_vc = False
 
 client.run(os.environ['TOKEN'])
