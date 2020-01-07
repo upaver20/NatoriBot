00d77e34f714e7fcdb95f5b5d697a8907d7d3d73
upaver20 <upa.ver.2.0@gmail.com>
2020-01-07 20:56:09 +0900

    軽微な修正

--- NatoriBot/response.py
+++ NatoriBot/response.py
@@ -9,14 +9,20 @@ async def voice_response(message, client):
     urls = Sanabutton2Parser(msg.content)
     if urls == None:
         reply = f"{msg.author.mention} {msg.content}は見つかりませんでした"
+        await msg.channel.send(reply)
+        await msg.delete()
         return
     else:
         reply = f"{msg.author.mention} \n 検索語句: {urls['msg']}\n アーカイブ: {urls['archive_url']}　\n ボタン: {urls['button_url']}"
-    await msg.channel.send(reply)
-    await msg.delete()
-    if bot.voice.is_connected() and not bot.voice.is_playing() and bot.is_in_vc:
-        download(urls['button_url'])
-        source = discord.FFmpegPCMAudio("button.mp3")
-        bot.voice.play(source)
+        await msg.channel.send(reply)
+        await msg.delete()
+
+    if bot.is_in_vc:
+        if bot.voice.is_connected() and not bot.voice.is_playing():
+            download(urls['button_url'])
+            source = discord.FFmpegPCMAudio("button.mp3")
+            bot.voice.play(source)
+        else:
+            return
     else:
         return
