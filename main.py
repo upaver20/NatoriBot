aba7b75e45f7f37d204063da9416e35846bbf047
upaver20 <upa.ver.2.0@gmail.com>
2020-01-28 03:27:00 +0900

    koreyana

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from NatoriBot.reaction import reaction
 client = discord.Client()
 
 if not discord.opus.is_loaded():
-    discord.opus.load_opus("heroku-buildpack-libopus")
+    discord.opus.load_opus('opus')
 
 
 @client.event
