3c9ec772b6cc3633aae990b496592f3594147960
upaver20 <upa.ver.2.0@gmail.com>
2020-10-26 21:51:08 +0900

    ヘルプを追加

--- NatoriBot/vc_access.py
+++ NatoriBot/vc_access.py
@@ -11,11 +11,30 @@ async def vc_access(message, client):
             reply = f"{msg.author.mention} 入室しました\n"
             print(reply)
             await msg.channel.send(reply)
-            await msg.delete()
 
         if msg.content.endswith('leave') and bot.is_in_vc:
             await bot.voice.disconnect()
             reply = f"{msg.author.mention} 退室しました\n"
             print(reply)
             await msg.channel.send(reply)
-            await msg.delete()
+
+        if msg.content.endswith('help') and bot.is_in_vc:
+            reply = \
+                f"さなボタン(2)様にある音声をDiscordに流すBotです．\n"\
+                f"<https://www.natorisana.love/>\n"\
+                f"\n"\
+                f"使い方：\n"\
+                f"`{client.user.mention} join`\n"\
+                f"あなたがいるボイスチャンネルに入室します．\n"\
+                f"`{client.user.mention} leave`\n"\
+                f"ボイスチャンネルから退室します．\n"\
+                f"`任意の文字列\n`"\
+                f"検索結果からランダムに音声を再生します．\n"\
+                f"`{client.user.mention} help`\n"\
+                f"これです．"
+            print(reply)
+            await msg.channel.send(reply)
+
+        await msg.delete()
+
+    return
