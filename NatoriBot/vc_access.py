04a1588c30f6aa8a621674c79d00f11e0185a01c
upaver20 <upa.ver.2.0@gmail.com>
2020-10-26 22:08:51 +0900

    helpフォーマットを更新

--- NatoriBot/vc_access.py
+++ NatoriBot/vc_access.py
@@ -24,13 +24,13 @@ async def vc_access(message, client):
                 f"<https://www.natorisana.love/>\n"\
                 f"\n"\
                 f"使い方：\n"\
-                f"`{client.user.mention} join`\n"\
+                f"```@{client.user} join```\n"\
                 f"あなたがいるボイスチャンネルに入室します．\n"\
-                f"`{client.user.mention} leave`\n"\
+                f"```@{client.user} leave```\n"\
                 f"ボイスチャンネルから退室します．\n"\
-                f"`任意の文字列\n`"\
+                f"```任意の文字列\n```"\
                 f"検索結果からランダムに音声を再生します．\n"\
-                f"`{client.user.mention} help`\n"\
+                f"```@{client.user} help```\n"\
                 f"これです．"
             print(reply)
             await msg.channel.send(reply)
