be7e81222b85ab1f6b6e25ca24ee10c5f18bf617
upaver20 <upa.ver.2.0@gmail.com>
2023-01-31 00:16:41 +0900

    posts.json からアーカイブ名を引っ張ってくるように変更

--- NatoriBot/Sanabutton2Parser.py
+++ NatoriBot/Sanabutton2Parser.py
@@ -7,26 +7,27 @@ def Sanabutton2Parser(message):
     msg = message
 
     baseURL = "https://www.natorisana.love/"
-    jsonURL = baseURL + "api/v1/buttons.json"
-    archive_list = json.loads(requests.get(jsonURL).text)
+
+    posts = json.load(open('data/posts.json', 'r'))
 
     cond_list = []
 
-    for archive in archive_list:
-        for block_archive in archive:
-            for button in block_archive:
-                if msg in str(button["value"]):
-                    cond_list.append(button)
+    for archive in posts:
+        if 'stream_id' in archive.keys():
+            for buttons in archive['buttons']:
+                for button in buttons:
+                    if msg in str(button['value']):
+                        cond_list.append(
+                            {"file-name": button['file-name'], "stream_id": archive['stream_id']})
 
     if cond_list == []:
         return None
 
     select_button = random.choice(cond_list)
 
-    buttonURL = baseURL + "sounds/" + select_button['file-name'] + ".mp3"
-
     urls = {
-            'button_url': buttonURL,
-            'msg': msg
-            }
+        'button_url': baseURL + "sounds/" + select_button['file-name'] + ".mp3",
+        'archive_url': "https://youtu.be/" + select_button['stream_id'],
+        'msg': msg
+    }
     return urls
